d196e384032dbcdc3b87ada7476d0c5ad7dd4ef0
xiezhenyu98 <751811152@qq.com>
2020-12-04 22:38:22 +0800

    fix:mybatis-plus分页查询拦截器

---
+++ src/main/java/com/xiezhenyu/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.xiezhenyu.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author Tim
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页拦截器插件
+     */
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+}
