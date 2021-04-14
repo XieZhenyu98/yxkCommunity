6b6f17da50966476c694638b7a208e5842c87d80
xiezhenyu98 <751811152@qq.com>
2021-04-14 09:12:36 +0800

    fix:修改controller调用

--- yx-community-admin/src/main/java/com/xiezhenyu/controller/LoginController.java
+++ yx-community-admin/src/main/java/com/xiezhenyu/controller/LoginController.java
@@ -2,8 +2,10 @@ package com.xiezhenyu.controller;
 
 import com.xiezhenyu.model.admin.UserManage;
 import com.xiezhenyu.response.CommonResult;
+import com.xiezhenyu.service.LoginService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin")
 public class LoginController {
 
+    @Autowired
+    private LoginService loginService;
+
     @ApiOperation(value = "管理员登录",notes = "管理员登录")
     @PostMapping("/login")
     public CommonResult login(@ApiParam("管理员登录") @RequestBody UserManage userManage){
-        return null;
+        return loginService.login(userManage);
     }
 }
